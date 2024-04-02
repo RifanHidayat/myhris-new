610df5bb82370052cd4dd3a5e0dc252ee84a5ec7
RifanHidayat <rifanhidayat0811@gmail.com>
2024-04-02 13:17:22 +0700

    update 02/04/2024

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -20,8 +20,8 @@ class Api {
 
   // static var basicUrl = "http://mobilehris.siscom.id:3000/";
   static var urlImage = 'https://imagehris.siscom.id:4431';
-  // static var basicUrl = "http://mobilehris.siscom.id:3000/";
-  static var basicUrl = "http://kantor.membersis.com:2629/";
+   static var basicUrl = "http://mobilehris.siscom.id:3000/";
+  // static var basicUrl = "http://kantor.membersis.com:2629/";
   static var token = '9d590c04119a4433971a1dd622266d38';
   static var luxand = 'https://api.luxand.cloud/photo/similarity';
   static var wappin = 'https://api.wappin.id/v1';
