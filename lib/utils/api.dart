85f74425ec40aa725f413253db0d938af8d60fe4
Rifan Hidayat <rifanhidayat0811@gmail.com>
2024-12-04 15:19:20 +0700

    update api

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -18,11 +18,11 @@ class Api {
       base64Encode(utf8
           .encode('aplikasioperasionalsiscom:siscom@ptshaninformasi#2022@'));
 
-  static var basicUrl = "http://mobilehris.siscom.id:3007/";
+  //static var basicUrl = "http://mobilehris.siscom.id:3007/";
   static var urlImage = 'https://imagehris.siscom.id:4431';
 
   //static var basicUrl = "http://mobilehris.siscom.id:3000/";
-  //static var basicUrl = "http://kantor.membersis.com:2626/";
+  static var basicUrl = "http://kantor.membersis.com:2626/";
 
   static var token = '9d590c04119a4433971a1dd622266d38';
   static var luxand = 'https://api.luxand.cloud/photo/similarity';
@@ -61,6 +61,7 @@ class Api {
 
     print("params" + params);
     // var getUrl = basicUrl + url + '?database=demohr' + params;
+
     var getUrl = basicUrl +
         url +
         "?database=${AppData.selectedDatabase}&start_periode=${AppData.startPeriode}&end_periode=${AppData.endPeriode}" +
